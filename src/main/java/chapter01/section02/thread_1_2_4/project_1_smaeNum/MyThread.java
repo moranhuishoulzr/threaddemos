f3c43bb1f2024b5684504c0073e8d523ab3a4727
Zuoran Liang(IT) <Zuoran.Liang@cicc.com.cn>
2019-07-02 19:10:57 +0800

    删除简单代码          虽然println()是同步的但是i--会出现线程安全问题

--- src/main/java/chapter01/section02/thread_1_2_4/project_1_smaeNum/MyThread.java
+++ src/main/java/chapter01/section02/thread_1_2_4/project_1_smaeNum/MyThread.java
@@ -1,7 +1,8 @@
 package chapter01.section02.thread_1_2_4.project_1_smaeNum;
 
-import java.beans.IntrospectionException;
-
+/**
+ * 测试目的：虽然printLn()内部是同步方法，但是i--却是在进入方法之前发生的，存在线程安全问题
+ */
 public class MyThread extends Thread {
 
 	private int i = 5;
